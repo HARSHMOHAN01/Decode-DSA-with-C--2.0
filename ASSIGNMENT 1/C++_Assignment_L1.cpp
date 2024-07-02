9564a543da92d2e3cbc3bae8d3cfe4ab8589c6c5
HARSH MOHAN <137272065+HARSHMOHAN01@users.noreply.github.com>
2024-07-02 11:00:01 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/C++_Assignment_L1.cpp
@@ -0,0 +1,50 @@
+//Q1
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Physics\n";
+    cout<<"Wallah";
+    return 0;
+}*/
+
+//Q2
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a=5,b=5,c=a+b;
+//     cout<<c;
+//     return 0;
+// }
+
+//Q3
+// int = 4 bytes
+// bool = 1 byte
+// float = 4 bytes
+
+//Q4
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a=4;
+//     int b=5;
+//     a++;b--;
+//     cout<<++a<<b--;
+//     return 0;
+// }
+
+
+//Q5
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int r=10,a;
+//     a=2*3.14*r;
+//     cout<<a;
+//     return 0;
+// }
+
+//Q6
