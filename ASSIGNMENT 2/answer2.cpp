c8d3f1cca59e341b95d67d183e4c8e6acf90e104
HARSH MOHAN <137272065+HARSHMOHAN01@users.noreply.github.com>
2024-07-02 19:03:28 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/answer2.cpp
@@ -0,0 +1,96 @@
+// //q1
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y,z,m;
+//     cout<<"Enter the first number = ";
+//     cin>>x;
+//     cout<<"Enter the second number = ";
+//     cin>>y;
+//     cout<<"Enter the modulus value = ";
+//     cin>>m;
+//     z=(x*y)/m;
+//     cout<<"the output is = "<<z;
+//     return 0;
+// }
+
+
+//q2
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x,y;
+//     cout<<"Enter the first number = ";
+//     cin>>x;
+//     cout<<"Enter the second number = ";
+//     cin>>y;
+//     cout<<(x!=y)<<" "<<(x>y);
+//     return 0;
+// }
+
+
+//q3
+//#include<iostream>
+//using namespace std;
+// int main()
+// {
+//     int x,y;
+//     cout<<"Enter the first number = ";
+//     cin>>x;
+//     cout<<"Enter the second number = ";
+//     cin>>y;
+//     x+=y;
+//     x-=y;
+//     x%=y;
+//     cout<<x;
+//     return 0;
+// }
+
+//q4
+
+//WAP to find volume of a cylinder
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int r,h,a;
+//     cout<<"Enter the radius of the cylinder = ";
+//     cin>>r;
+//     cout<<"Enter the height of the cylinder = ";
+//     cin>>h;
+//     a=3.14*r*r*h;
+//     cout<<"The volume of the cylinder is = "<<a;
+//     return 0;
+// }
+
+//q5
+
+//WAP to find the difference between two ascii characters.
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char x,y;
+//     cout<<"Request to please enter a character = ";
+//     cin>>x;
+//     cout<<"Request to please enter second character = ";
+//     cin>>y;
+//     int a=(int)x;
+//     int b=(int)y;
+//     cout<<"The difference between both the ASCII character is = "<<a-b;
+//     return 0;
+// }
+
+
+//q6
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int i = ( 4 + 7 / 5 * 6 * 6+9 )% 100 ;
+//     cout<<i;
+// }
